d8172e4d16e3622d3a4c1fc4dc96a162f439815e
choihaaz <choihaaz@gmail.com>
2025-10-10 00:33:18 -0400

    chore: add test dependencies (Mockk, Coroutine Test)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,9 +58,12 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
